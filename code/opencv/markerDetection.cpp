24b0b83c62e037c8a8b786fd40ba149a46fbe0db
Hwei-Shin Harriman <hwei.shin.harriman@gmail.com>
2019-05-08 19:27:07 -0400

    added compile instructions for aruco-version2

---
+++ code/opencv/markerDetection.cpp
@@ -0,0 +1,34 @@
+//Run with:
+// $ g++ -o arucodet markerDetection.cpp
+#include <opencv2/aruco.hpp>
+#include <iostream>
+
+using namespace std;
+
+cv::VideoCapture inputVideo;
+inputVideo.open(0);
+cv::Mat cameraMatrix, distCoeffs;
+// camera parameters are read from somewhere
+readCameraParameters(cameraMatrix, distCoeffs);
+cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+while (inputVideo.grab()) {
+    cv::Mat image, imageCopy;
+    inputVideo.retrieve(image);
+    image.copyTo(imageCopy);
+    std::vector<int> ids;
+    std::vector<std::vector<cv::Point2f>> corners;
+    cv::aruco::detectMarkers(image, dictionary, corners, ids);
+    // if at least one marker detected
+    if (ids.size() > 0) {
+        cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        std::vector<cv::Vec3d> rvecs, tvecs;
+        cv::aruco::estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+        // draw axis for each marker
+        for(int i=0; i<ids.size(); i++)
+            cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+    }
+    cv::imshow("out", imageCopy);
+    char key = (char) cv::waitKey(waitTime);
+    if (key == 27)
+        break;
+}
