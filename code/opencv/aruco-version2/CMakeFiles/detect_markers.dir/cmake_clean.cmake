f749d57c410a43af640c782f706257f3a2aa640e
coverney <cassandra.overney@students.olin.edu>
2019-05-09 11:28:11 -0400

    got detect marker working

---
+++ code/opencv/aruco-version2/CMakeFiles/detect_markers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_markers.dir/detect_markers.cpp.o"
+  "detect_markers.pdb"
+  "detect_markers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_markers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- code/opencv/aruco-version2/markerDetection.cpp
+++ code/opencv/aruco-version2/markerDetection.cpp
@@ -2,31 +2,36 @@
 #include <iostream>
 
 using namespace std;
+using namespace cv;
 
-cv::VideoCapture inputVideo;
-inputVideo.open(0);
-cv::Mat cameraMatrix, distCoeffs;
-// camera parameters are read from somewhere
-readCameraParameters(cameraMatrix, distCoeffs);
-cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-while (inputVideo.grab()) {
-    cv::Mat image, imageCopy;
-    inputVideo.retrieve(image);
-    image.copyTo(imageCopy);
-    std::vector<int> ids;
-    std::vector<std::vector<cv::Point2f>> corners;
-    cv::aruco::detectMarkers(image, dictionary, corners, ids);
-    // if at least one marker detected
-    if (ids.size() > 0) {
-        cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-        std::vector<cv::Vec3d> rvecs, tvecs;
-        cv::aruco::estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
-        // draw axis for each marker
-        for(int i=0; i<ids.size(); i++)
-            cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
-    }
-    cv::imshow("out", imageCopy);
-    char key = (char) cv::waitKey(waitTime);
-    if (key == 27)
-        break;
+int main(int argc, char const *argv[]) {
+  cv::VideoCapture inputVideo;
+  inputVideo.open(0);
+  cv::Mat cameraMatrix, distCoeffs;
+  // camera parameters are read from somewhere
+  // cvLoadWindowParameters(cameraMatrix, distCoeffs);
+  // readCameraParameters(cameraMatrix, distCoeffs);
+  cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+  while (inputVideo.grab()) {
+      cv::Mat image, imageCopy;
+      inputVideo.retrieve(image);
+      image.copyTo(imageCopy);
+      std::vector<int> ids;
+      std::vector<std::vector<cv::Point2f>> corners;
+      cv::aruco::detectMarkers(image, dictionary, corners, ids);
+      // if at least one marker detected
+      if (ids.size() > 0) {
+          cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+          std::vector<cv::Vec3d> rvecs, tvecs;
+          cv::aruco::estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+          // draw axis for each marker
+          for(int i=0; i<ids.size(); i++)
+              cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+      }
+      cv::imshow("out", imageCopy);
+      char key = (char) cv::waitKey(-1);
+      if (key == 27)
+          break;
+  }
+  return 0;
 }
